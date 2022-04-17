2ed5ef3594faf23e21eadf2783acaf1917e87b05
keitoyokoi0628 <d51makbbcf@gmail.com>
2022-04-17 06:33:00 +0000

    管理者:ユーザーの勤怠一覧情報をCSV出力できるようになりました

--- app/controllers/admin/attendances_controller.rb
+++ app/controllers/admin/attendances_controller.rb
@@ -1,5 +1,6 @@
 class Admin::AttendancesController < ApplicationController
   layout 'admin'
+  require 'csv'
 
   def top
     @attendances = Attendance.where(start: Time.zone.now.all_day)
@@ -12,6 +13,12 @@ class Admin::AttendancesController < ApplicationController
   def show
     @customer = Customer.find(params[:id])
     @attendance = @customer.attendances
+    respond_to do |format|
+      format.html
+      format.csv do |csv|
+        send_attendances_csv(@attendance)
+      end
+    end
   end
 
   def edit
@@ -38,6 +45,22 @@ class Admin::AttendancesController < ApplicationController
 
   def attendance_params
     params.permit(:start, :customer_id, :location, :is_active, :re_start, :re_end, :cause, :re_location )
+  end
+
+  def send_attendances_csv(attendances)
+    csv_data = CSV.generate do |csv|
+      column_names = %w(出勤時刻 退勤時刻 勤務地)
+      csv << column_names
+      attendances.each do |attendance|
+        column_values = [
+          attendance.start,
+          attendance.end,
+          attendance.location,
+        ]
+        csv << column_values
+      end
+    end
+    send_data(csv_data, filename: "勤怠一覧.csv")
   end
 
 end
